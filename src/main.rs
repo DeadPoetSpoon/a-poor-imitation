7b47b2cc6892a5904f131f281ac87d6a8e5132fb
Dead Poet Spoon <deadpoetspoon@qq.com>
2025-08-16 23:20:00 +0800

    test: update bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::log::{debug, info};
 use bevy::prelude::*;
 
 #[derive(Resource)]
@@ -14,34 +15,27 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         // add things to your app here
-        app
-            .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_systems(Startup, add_people)
             .add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
 fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins, HelloPlugin))
-        .run();
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
 }
 
 fn update_people(mut query: Query<&mut Name, With<Person>>) {
     for mut name in &mut query {
         if name.0 == "Elaina Proctor" {
+            info!("change people name");
             name.0 = "Elaina Hume".to_string();
             break; // We don’t need to change any other names
         }
     }
 }
 
-
-fn greet_people(
-    time: Res<Time>,
-    mut timer: ResMut<GreetTimer>,
-    query: Query<&Name, With<Person>>
-) {
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
     // update our timer with the time elapsed since the last update
     // if that caused the timer to finish, we say hello to everyone
     if timer.0.tick(time.delta()).just_finished() {
@@ -51,8 +45,8 @@ fn greet_people(
     }
 }
 
-
 fn add_people(mut commands: Commands) {
+    debug!("add 3 people");
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
